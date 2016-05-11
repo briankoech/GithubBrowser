acd53732e372c08d499cb027810a31d51273ed27
Brian Koech <brnkirwa@gmail.com>
2016-05-11 19:57:05 +0300

    Add login authentication

--- Login.js
+++ Login.js
@@ -1,14 +1,68 @@
 
-import React, { Component, Text, StyleSheet, View, Image, TextInput, TouchableHighlight } from 'react-native';
+import React, { 
+  Component,
+  Text, 
+  StyleSheet, 
+  View, 
+  Image, 
+  TextInput, 
+  TouchableHighlight,
+ActivityIndicatorIOS } from 'react-native';
+import buffer from 'buffer';
 
 export default class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showProgress: false
+    };
   }
-  onLogginPressed = () => {
+  onLogginPressed() {
     console.log(this.state.username, this.state.password);
-  };
+    this.setState({showProgress: true});
+
+    var b = new buffer.Buffer(this.state.username + 
+      ':' + this.state.password);
+    var encodedAuth = b.toString('base64');
+
+    fetch('https://api.github.com/user', {
+      headers: {
+        'Authorization': 'Basic ' + encodedAuth
+      }
+    })
+    .then((response)=> {
+      if(response.status >= 200 && response.status < 300) {
+        return response;
+      }
+
+      throw {
+        badCredentials: response.status == 401,
+        unknownError: response.status != 401
+      };
+    })
+    .then((results)=> {
+      console.log(results);
+      this.setState({success: true});
+    })
+    .catch((err) => {
+      this.setState(err);
+    })
+    .finally(() => {
+      this.setState({showProgress: false});
+    })
+  }
   render() {
+    var errorCtrl = <View />;
+    if(!this.state.success && this.state.badCredentials) {
+      errorCtrl = <Text style={styles.error}>
+        That username and password combination did not work
+      </Text>;
+    }
+    if(!this.state.success && this.state.unknownError) {
+      errorCtrl = <Text style={styles.error}>
+        We experienced an unexpected issue
+      </Text>;
+    }
     return (
       <View style={styles.container}>
         <Image style={styles.logo} 
@@ -18,23 +72,31 @@ export default class Login extends Component {
           Github browser
         </Text>
         <TextInput 
-        onChangeText={(text) => {this.setState({username: text})}}
-        style={styles.input}
-        placeholder="Github username">
+          onChangeText={(text) => {this.setState({username: text})}}
+          style={styles.input}
+          placeholder="Github username">
         </TextInput>
         <TextInput
-        onChangeText={(text) => {this.setState({password: text})}}
-        style={styles.input}
-        placeholder="Github password"
-        secureTextEntry="true">
+          onChangeText={(text) => {this.setState({password: text})}}
+          style={styles.input}
+          placeholder="Github password"
+          secureTextEntry="true">
         </TextInput>
         <TouchableHighlight 
-        onPress={this.onLogginPressed}
-        style={styles.button}>
+          onPress={this.onLogginPressed.bind(this)}
+          style={styles.button}>
           <Text style={styles.buttonText}>
             Log in
           </Text>
         </TouchableHighlight>
+
+        {errorCtrl}
+
+        <ActivityIndicatorIOS 
+          animating={this.state.showProgress}
+          size="large" 
+          style={styles.loader}
+          />
       </View>
     ); 
   }
@@ -75,5 +137,12 @@ var styles = StyleSheet.create({
     fontSize: 22,
     color: '#FFF',
     alignSelf: 'center'
+  },
+  loader: {
+    marginTop: 20
+  },
+  error: {
+    color: 'red',
+    paddingTop: 10
   }
-})
+});
